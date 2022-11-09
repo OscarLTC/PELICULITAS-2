b955949f0a14f47d0eeaa52e5195ab36f6bcb01e
Oscar <oscartaco2003@gmail.com>
2022-11-09 11:59:35 -0500

    [add] i'm stuck

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -2,25 +2,36 @@ import { RiMovie2Fill } from 'react-icons/ri'
 import { BiHomeAlt, BiWorld, BiUser } from 'react-icons/bi'
 import { CiSearch } from 'react-icons/ci'
 import { HiXMark, HiOutlineBars3 } from 'react-icons/hi2'
-
-import './header.css'
 import { useState } from 'react'
+import './header.css'
+import { useEffect } from 'react'
 export const Header = () => {
-    const options = [
+    const [options, setOptions] = useState([
         {
+            id: 1,
             name: 'Home',
             icon: <BiHomeAlt className='option-icon' />,
+            isSelected: true,
         },
         {
+            id: 2,
             name: 'Discover',
             icon: <BiWorld className='option-icon' />,
+            isSelected: false,
         },
         {
+            id: 3,
             name: 'User',
             icon: <BiUser className='option-icon' />,
+            isSelected: false,
         },
-    ]
+    ])
+
     const [icon, setIcon] = useState(false)
+
+    useEffect(() => {
+        setOptions(options)
+    }, [options])
 
     return (
         <div className='header'>
@@ -29,12 +40,36 @@ export const Header = () => {
                     <RiMovie2Fill />
                     <div> Peliculitas</div>
                 </div>
+                <div className='searchtop'>
+                    <CiSearch className='icon' />
+                    <input
+                        type='text'
+                        className='text'
+                        placeholder={'Search'}
+                    />
+                </div>
                 <div className='icons' onClick={(e) => setIcon(!icon)}>
                     {!icon ? (
                         <HiOutlineBars3 className='menu-icon' />
                     ) : (
                         <HiXMark className='menu-icon' />
                     )}
+                </div>
+                <div className='optionstop'>
+                    <ul>
+                        {options.map((o) => (
+                            <li
+                                key={o.id}
+                                onClick={() => (o.isSelected = true)}
+                                className={
+                                    o.isSelected ? 'option-selected' : ''
+                                }
+                            >
+                                <div className='icon-option'>{o.icon}</div>
+                                <div className='name-option'>{o.name}</div>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
             <div className={icon ? 'menuv' : 'menui'}>
